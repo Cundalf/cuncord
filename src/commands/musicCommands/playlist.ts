901b6978928a412bea61caa4d4a20f28bbfda855
Agustin Cundari <acundari95@gmail.com>
2022-12-13 01:37:01 -0300

    Mejora feedback al agregar una playlist

--- src/commands/musicCommands/playlist.ts
+++ src/commands/musicCommands/playlist.ts
@@ -14,17 +14,27 @@ export default class PlaylistCommand extends BaseCommand {
             const guildQueue = player.getQueue(guild.id);
             const queue = player.createQueue(guild.id);
             const playlist = interaction.options.getString('playlist') as string;
+            const channel = interaction.channel;
 
-            await interaction.reply('Adding... Please, wait Senpai :(');
-            await queue.join(guildMember.voice.channel as VoiceBasedChannel);
-            // const playlistInQueue =
-            await queue.playlist(playlist)
-                .catch(err => {
-                    console.log(err);
-                    if (!guildQueue) {
-                        queue.stop();
-                    }
-                });
+            try {
+                await interaction.reply('Adding... Please, wait Senpai :(');
+                await queue.join(guildMember.voice.channel as VoiceBasedChannel);
+
+                // const playlistInQueue =
+                await queue.playlist(playlist)
+                    .catch(err => {
+                        console.log(err);
+                        if (!guildQueue) {
+                            queue.stop();
+                        }
+                    });
+
+                const username = interaction.user.username;
+                const cantSongs = queue.songs.length;
+                await channel?.send(`${username} added ${cantSongs} songs`);
+            } catch (err) {
+                console.log(err);
+            }
         }
     }
 }
